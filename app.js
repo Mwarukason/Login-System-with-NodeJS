05dcdc58e12fd90933fec60f235e316819c1ece0
mwarukason <a.mwaruka93@gmail.com>
2017-12-19 22:28:25 +0800

    add folders

---
+++ app.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var localStrategy = require('passport-local'),Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/loginapp');
+var db = mongoose.connection;
+
+//routes
+var routes = require('./routes/index');
+var users = require('./routes/users')
+
+//initialise the app
+var app = express();
+
+//view engine
+app.set('views', path.join( __dirname, 'views'));
+app.engine('handlebars', exphbs({ defaultLayout: 'layout' }));
+app.set('view engine', 'handlebars');
+
+// BodyParser Midleware for setups
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+
+// Set public/static folder
+app.use(express.static(path.join( __dirname, 'public')));
+
+// Express session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave:true
+}));
+
+// Passport initialise
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value){
+    var namespace = param.split('.')
+    , root = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length){
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return{
+      param: formParam,
+      msg  :  msg,
+      value :value
+    };
+  }
+}));
+
+//flash middleware
+app.use(flash());
+
+// Global Var
+app.use(function(req, res, next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+//route middleware
+app.use('/', routes);
+app.use('/users', users);
+
+//set passport
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+  console.log('Server has started on port '+app.get('port'));
+})
